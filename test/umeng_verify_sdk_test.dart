d869f96f7aa4b10c1923bd6d6fcb3aadb4a7a701
tangchi-looki <tangchi@looki.ai>
2026-02-28 17:59:24 +0800

    umeng_verify_sdk_2.0.0

---
+++ test/umeng_verify_sdk_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/umeng_verify_sdk.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('umeng_verify_sdk');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await UmengVerifySdk.VerifyVersion, '42');
+  });
+}
